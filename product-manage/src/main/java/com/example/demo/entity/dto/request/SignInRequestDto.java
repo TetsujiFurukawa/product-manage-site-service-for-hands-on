6379f08292ee3bd925132472d1650f9edb6535ee
TetsujiFurukawa <tetsufuru1968@yahoo.co.jp>
2021-01-28 00:53:31 +0900

    Added google formatter settings.

--- product-manage/src/main/java/com/example/demo/entity/dto/request/SignInRequestDto.java
+++ product-manage/src/main/java/com/example/demo/entity/dto/request/SignInRequestDto.java
@@ -2,7 +2,6 @@ package com.example.demo.entity.dto.request;
 
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
-
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
